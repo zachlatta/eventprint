abd737dce4b7163c4d568a0e11d292ce35475fe1
Zach Latta <zach@zachlatta.com>
2014-04-05 16:00:33 -0700

    Set up AngularJS.

---
+++ server/public/script/controllers/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('eventprint')
+  .controller('MainCtrl', function ($scope) {
+  });
---
+++ server/public/script/controllers/nav.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('eventprint')
+  .controller('NavCtrl', function ($scope) {
+  });
