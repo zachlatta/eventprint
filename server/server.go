62798a32eb91b436b147fdcd9b5271a0193cb0a0
Zach Latta <zach@zachlatta.com>
2014-04-04 23:40:06 -0700

    Set up Gorp.

--- server/server.go
+++ server/server.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/coopernurse/gorp"
+	"github.com/go-martini/martini"
+)
 
 func main() {
 	m := martini.Classic()
 
 	m.Use(martini.Static("public"))
+	m.MapTo(Dbm, (*gorp.SqlExecutor)(nil))
 
 	m.Group("/api", func(r martini.Router) {
 		r.Get("/hello_world", func() string {
