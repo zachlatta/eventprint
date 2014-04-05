f52a99c69cac051c404b7105ea4da34d8ce1986a
Zach Latta <zach@zachlatta.com>
2014-04-05 15:03:45 -0700

    Log errors to logger in GetAttendee.

--- server/route/attendee.go
+++ server/route/attendee.go
@@ -17,11 +17,13 @@ func GetAttendee(db gorp.SqlExecutor, w http.ResponseWriter,
 
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
+		log.Printf("Error converting id %d to string: %v\n", params["id"], err)
 		return http.StatusBadRequest, "Id not an int"
 	}
 
 	obj, err := db.Get(model.Attendee{}, id)
 	if err != nil {
+		log.Printf("Error retrieving attendee with id %d: %v\n", id, err)
 		return http.StatusNotFound, ""
 	}
 
@@ -29,12 +31,10 @@ func GetAttendee(db gorp.SqlExecutor, w http.ResponseWriter,
 
 	json, err := json.Marshal(attendee)
 	if err != nil {
+		log.Println("Error marshalling attendee to JSON:", err)
 		return http.StatusInternalServerError, "Error when retrieving attendee"
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	return http.StatusOK, string(json)
-}
-
-func SignInAttendee(db gorp.SqlExecutor) {
 }
