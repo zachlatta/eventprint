6cd99484e568d822c84a12d24b67c158e645ca80
Zach Latta <zach@zachlatta.com>
2014-04-06 17:32:17 -0700

    Improve badge generation output.

--- client/main.go
+++ client/main.go
@@ -7,12 +7,16 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	_ "image/jpeg"
+	_ "image/png"
 	"io"
 	"log"
+	"math"
 	"net"
 	"net/url"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/adkennan/dox2go"
@@ -26,10 +30,14 @@ const (
 	pongWait = 60 * time.Second
 )
 
-var baseUrl string
+var (
+	baseUrl  string
+	logoPath string
+)
 
 func main() {
 	flag.StringVar(&baseUrl, "url", "http://localhost:3000", "base url of server")
+	flag.StringVar(&logoPath, "logo", "lahacks.png", "path to hackathon logo")
 	flag.Parse()
 
 	url, err := url.Parse(baseUrl + "/api/ws")
@@ -84,7 +92,7 @@ func GeneratePDF(attendee model.Attendee) (string, error) {
 
 	doc := pdf.NewPdfDoc(&b)
 
-	pWidth, pHeight := dox2go.StandardSize(dox2go.PS_A7, dox2go.U_MM)
+	pWidth, pHeight := dox2go.StandardSize(dox2go.PS_A8, dox2go.U_MM)
 	page := doc.CreatePage(dox2go.U_MM, pWidth, pHeight, dox2go.PO_Portrait)
 
 	s := page.Surface()
@@ -94,23 +102,33 @@ func GeneratePDF(attendee model.Attendee) (string, error) {
 		return "", err
 	}
 
-	name := doc.CreateFont(pdf.FONT_Helvetica, dox2go.FS_Bold, 12)
+	logoImage, err := LoadImageFromFile(logoPath)
+	if err != nil {
+		return "", err
+	}
+
+	nameSize := (pWidth + 25) / math.Max(float64(len(attendee.FirstName)),
+		float64(len(attendee.LastName)))
+	name := doc.CreateFont(pdf.FONT_Helvetica, dox2go.FS_Bold, nameSize)
 	typeOfAdmissionPath := dox2go.NewPath()
-	typeOfAdmission := doc.CreateFont(pdf.FONT_Helvetica, dox2go.FS_Bold, 10)
+	typeOfAdmission := doc.CreateFont(pdf.FONT_Helvetica, dox2go.FS_Bold, 6)
 	qrcode := doc.CreateImage(qrImage)
+	logo := doc.CreateImage(*logoImage)
+
+	s.Image(logo, 18, 55, 45, 45)
 
 	s.Bg(dox2go.RGB(0, 0, 0))
-	s.Text(name, 17, 75, attendee.FirstName)
-	s.Text(name, 0, -12, attendee.LastName)
+	s.Text(name, 0, 37, attendee.LastName)
+	s.Text(name, 0, nameSize, attendee.FirstName)
 
-	s.Image(qrcode, 23, 30, 75, 75)
+	s.Image(qrcode, 18, 15, 50, 50)
 
 	s.Fg(dox2go.RGB(0, 0, 0))
-	typeOfAdmissionPath.Rect(5, 5, 69, 20)
+	typeOfAdmissionPath.Rect(0, 0, 52, 10)
 	s.Fill(typeOfAdmissionPath)
 
 	s.Bg(dox2go.RGB(255, 255, 255))
-	s.Text(typeOfAdmission, 11, 9, attendee.Title)
+	s.Text(typeOfAdmission, 3, 3, strings.ToUpper(attendee.Title))
 
 	doc.Close()
 
@@ -140,4 +158,19 @@ func CreateQRCode(text string) (image.Image, error) {
 	}
 
 	return qrcode, nil
+}
+
+func LoadImageFromFile(path string) (*image.Image, error) {
+	reader, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
 }
