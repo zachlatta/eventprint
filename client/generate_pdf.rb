56cbf6e05ef1915a330d132ca1e6ac50e5b9b380
Shrav Mehta <shravvmehtaa@gmail.com>
2014-04-08 23:17:57 -0700

    ruby script

--- client/generate_pdf.rb
+++ client/generate_pdf.rb
@@ -14,19 +14,19 @@ LAST_NAME = ARGV[2]
 TITLE = ARGV[3]
 HACKATHON_LOGO_PATH = 'lahacks.png'
 FILE_NAME = ARGV[4]
- 
-Prawn::Document.generate(FILE_NAME, page_size: [200, 350]) do
+
+Prawn::Document.generate(FILE_NAME, page_size: [144, 252]) do
   font 'Helvetica'
-  pad_top(112) do
-    image HACKATHON_LOGO_PATH, width: 90, at: [20,275]
-    text FIRST_NAME, style: :bold, size: 24, align: :center, top_margin: '100'
-    text LAST_NAME, style: :bold, size: 24, align: :center
+  pad_top(75) do
+    image HACKATHON_LOGO_PATH, width: 70, at: [0,190]
+    text FIRST_NAME, style: :bold, size: 12, align: :center, top_margin: '40'
+    text LAST_NAME, style: :bold, size: 12, align: :center
   end
-  pad_top(2) do
-    print_qr_code(BARCODE, extent: 100, align: :center)
+  pad_top(5) do
+    print_qr_code(BARCODE, extent: 72, align: :center)
   end
-  fill { rectangle [-36,10], 200, 50 }
-  bounding_box([-36,-3], width: 200, height: 40) do
-    text TITLE, style: :bold, size: 20, color: 'ffffff', align: :center
+  fill { rectangle [-36,-6], 144, 30 }
+  bounding_box([-36,-13], width: 144, height: 40) do
+    text TITLE, style: :bold, size: 16, color: 'ffffff', align: :center
   end
 end
