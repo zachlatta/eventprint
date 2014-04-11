f5dd4104a4b785ecc00cf41ea791975e7dbfc9ee
Zach Latta <zach@zachlatta.com>
2014-04-11 12:58:29 -0700

    Fix pagination woes.

--- server/public/script/controllers/main.js
+++ server/public/script/controllers/main.js
@@ -2,22 +2,23 @@
 
 angular.module('eventprint')
   .controller('MainCtrl', function ($scope, $filter, Attendee) {
+    $scope.paginatedAttendees = [];
     $scope.filteredAttendees = [];
     $scope.attendees = Attendee.query(function () {
-      $scope.filterAttendees();
+      $scope.filterAndPaginateAttendees();
     });
     $scope.alerts = [];
     $scope.currentPage = 1;
     $scope.pageSize = 25;
     $scope.maxPaginationButtons = 7;
 
-    $scope.filterAttendees = function () {
+    $scope.filterAndPaginateAttendees = function () {
       var begin = (($scope.currentPage - 1) * $scope.pageSize),
           end = begin + $scope.pageSize;
 
       $scope.filteredAttendees = $filter('filter')($scope.attendees, $scope.query);
       $scope.filteredAttendees = $filter('orderBy')($scope.filteredAttendees, 'lastName');
-      $scope.filteredAttendees = $scope.filteredAttendees.slice(begin, end);
+      $scope.paginatedAttendees = $scope.filteredAttendees.slice(begin, end);
     };
 
     $scope.sync = function () {
@@ -40,9 +41,9 @@ angular.module('eventprint')
       $scope.alerts.splice(index, 1);
     };
 
-    $scope.$watch('currentPage + pageSize', $scope.filterAttendees);
+    $scope.$watch('currentPage + pageSize', $scope.filterAndPaginateAttendees);
     $scope.$watch('query', function() {
       $scope.currentPage = 1;
-      $scope.filterAttendees();
+      $scope.filterAndPaginateAttendees();
     });
   });
